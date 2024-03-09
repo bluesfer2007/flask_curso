01d3d774f600003b3507fd2c0543020e0efa91fa
bluesfer <bluesfer2007@gmail.com>
2024-03-08 20:30:01 -0500

    add img and template to error and nav bar

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from flask import Flask, request, make_response, redirect, render_template
 
 #variable de tipo lista para iterar
-todos= ['todo1', 'todo2', 'todo3', 'todo4']
+todos= ['Limpiar casa', 'Comida gatos', 'Agua plantas', 'Sacar basura']
 #iniciar el app nombre de aplicacion igual al nombre del archivo
 app = Flask(__name__)
 #esto es el arranque de la ruta con @app.route colocas las rutas para despligue
 #en este punto deberia arranca
 #esta el la ruta raiz
+#CAPTURAR ERRORES DE CARGA O SERVIDOR
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+#funcion para cachear error 500 de server error
+@app.errorhandler(500)
+def not_server_work(error):
+    return render_template('500.html',error=error)
+
+
 @app.route('/')
 def index():
     #esto es un metodo de request
